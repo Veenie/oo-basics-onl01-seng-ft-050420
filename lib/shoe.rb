5788a3f99b57eccaadcdec730e7c4e7bf4744439
Raveen K Sharma <raveen08@gmail.com>
2020-05-13 20:22:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,25 +8,7 @@ def initialize(brand)
     @brand = brand
   end
  
-  def brand
-    @brand
-  end
- 
-  def color=(color)
-    @color = color
-  end
- 
-  def color
-    @color
-  end
- 
-  def size=(size)
-    @size = size
-  end
- 
-  def size
-    @size
-  end
+
   
   def condition=(x)
     @condition = x
