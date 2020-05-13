d9a514ecfc3b1f15be475b3f80f177b102bb3008
Raveen K Sharma <raveen08@gmail.com>
2020-05-13 20:26:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,25 +8,7 @@ def initialize(brand)
     @brand = brand
   end
  
-  def brand
-    @brand
-  end
- 
-  def color=(color)
-    @color = color
-  end
- 
-  def color
-    @color
-  end
- 
-  def size=(size)
-    @size = size
-  end
- 
-  def size
-    @size
-  end
+
   
   def condition=(x)
     @condition = x
@@ -46,7 +28,7 @@ def initialize(brand)
  
   def cobble
     @condition = "new"
-    puts "Your shoe is as good as new!!"
+    puts "Your shoe is as good as new!"
   end
 
 
